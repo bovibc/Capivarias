a5ee404b1818739b6be205ff6ad992c00544c6dd
Clissia Bovi <bovibc@gmail.com>
2023-10-20 08:44:21 -0300

    parameters

--- Capivarias/Capivarias/Screen Elements/Characters/Capybara.swift
+++ Capivarias/Capivarias/Screen Elements/Characters/Capybara.swift
@@ -6,7 +6,30 @@
 //
 
 import Foundation
+import SpriteKit
 
 class Capybara {
 
+    private var life: Float = 100
+    private var breathTime: Float = 100
+    private var speed: Float = 3.0
+    private var defense: Float = 100
+    private var assetScale: CGFloat = 0.09
+    private var stoppedImage: String = "capybara_stopped"
+
+    var sprite: SKSpriteNode
+
+    init() {
+        self.sprite = SKSpriteNode(imageNamed: stoppedImage)
+    }
+
+    func startCapybara(screenWidth: CGFloat, screenHeight: CGFloat) {
+        let scale = screenWidth * assetScale / sprite.size.width
+        let texture = SKTexture(imageNamed: "capybara_stopped")
+        sprite.physicsBody = SKPhysicsBody(texture: texture, size: texture.size())
+        sprite.physicsBody?.affectedByGravity = false
+        sprite.position = CGPoint(x: 200, y: screenHeight/2)
+        sprite.zPosition = 10
+        sprite.setScale(scale)
+    }
 }
