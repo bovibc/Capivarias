a181dbccfbc38e106b9ef0c65cfd913c94078512
Renan Tavares <renan_velox@hotmail.com>
2023-10-25 08:26:25 -0300

    atributos das armas

---
+++ Capivarias/Capivarias/Screen Elements/Weapons/Weapons.swift
@@ -0,0 +1,27 @@
+//
+//  Weapons.swift
+//  Capivarias
+//
+//  Created by Renan Tavares on 24/10/23.
+//
+
+import Foundation
+
+
+enum TypeWeapon {
+ //   case 
+}
+
+
+
+class Weapons {
+    var typeWeapon = "sword"
+    var damage = 7
+    var range = 3.0
+    
+    
+    
+    
+    func changeSword()
+    
+}
