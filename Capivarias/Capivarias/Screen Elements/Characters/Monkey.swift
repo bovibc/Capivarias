46570f87d645fe5dc3b3265df96f1b5acec28c98
Renan Tavares <renan_velox@hotmail.com>
2023-11-13 09:07:02 -0300

    criando a classe do macaco

--- Capivarias/Capivarias/Screen Elements/Characters/Monkey.swift
+++ Capivarias/Capivarias/Screen Elements/Characters/Monkey.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import SpriteKit
+
+
+class Monkey {
+    var life: Float = 100
+    private var damage: Float = 20
+    private var speed: CGFloat = 2
+    private var attackSpeed: CGFloat = 1
+    private let staticName: String = "a1"
+    private var isMonkeyWalking: Bool = false
+    private let movementMonkeyName: String = "Monkey_Waking"
+    private let speedAtack: Float = 3.0
+    var sprite: SKSpriteNode
+    var range: Float = 5
+    
+    init() {
+        self.sprite = SKSpriteNode(imageNamed: staticName)
+    }
+}
