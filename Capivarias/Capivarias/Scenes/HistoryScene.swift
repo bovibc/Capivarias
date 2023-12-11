cabbe8264d55f843b4f98aeb57b70eae4dc0a71e
Clissia Bovi <bovibc@gmail.com>
2023-12-11 09:52:45 -0300

    history

--- Capivarias/Capivarias/Scenes/HistoryScene.swift
+++ Capivarias/Capivarias/Scenes/HistoryScene.swift
@@ -17,65 +17,48 @@ class HistoryScene: SKScene {
     let assets = Assets()
     var history: [String] = []
     var currentDialogueindex = 0
-    
+    var removed: Bool = false
+
     override func didMove(to view: SKView) {
         print("didmove")
         setupBackground()
         setup()
         showNextDialogue()
     }
-    
+
     override func update(_ currentTime: TimeInterval) {
         if currentDialogueindex > 0 {
-            
+            guard removed else { return }
             backButton = SKSpriteNode(imageNamed: assets.seta)
-            guard let backButton else { return}
-            backButton.name = "backButton"
-            backButton.position = CGPoint(x: (size.width * 0.05), y: size.height / 2)
-            backButton.xScale = -1.0
-            
-            addChild(backButton)
+            backButton?.name = "backButton"
+            backButton?.position = CGPoint(x: (size.width * 0.05), y: size.height / 2)
+            backButton?.xScale = -1.0
+            removed = false
+            addChild(backButton ?? SKSpriteNode())
         } else {
-            guard let backButton else { return}
-            backButton.removeFromParent()
+            guard !removed else { return }
+            self.backButton?.removeFromParent()
+            removed = true
         }
     }
-    
+
     private func setupBackground() {
         backgroundController.setupBackground(scene: self, imageName: assets.historyMap)
     }
-    
+
     func setup() {
         print("setup")
         historyImage = SKSpriteNode(imageNamed: assets.history[currentDialogueindex])
         historyImage.position = CGPoint(x: size.width / 2, y: size.height / 2)
         historyImage.size = size
         addChild(historyImage)
-        
-        
+
         nextButton = SKSpriteNode(imageNamed: assets.seta)
         nextButton.position = CGPoint(x: size.width * 0.955, y: size.height / 2)
         nextButton.name = "nextButton"
         addChild(nextButton)
     }
-    
-    func showNextDialogue() {
-        if currentDialogueindex < assets.history.count {
-            guard let historyImage else {return}
-            // historyImage.texture = SKTexture(imageNamed: "dialogueImage\(currentDialogueIndex + 1)")
-            historyImage.texture = SKTexture(imageNamed: assets.history[currentDialogueindex])
-            print(historyImage.texture)
-        } else {
-            // Todas as falas foram exibidas, faça algo aqui, como transição para a próxima cena.
-            if let view = self.view {
-                transactionScene.goToNextLevel(view: view, gameScene: "FirstScene")
-            }
-           
-        }
-    }
-    
-    
-    
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
        // guard let backButton else { return}
         for touch in touches {
@@ -85,16 +68,24 @@ class HistoryScene: SKScene {
                 currentDialogueindex += 1
                 showNextDialogue()
             } else if ((backButton?.contains(location)) != nil) {
-                if currentDialogueindex > 0{
+                if currentDialogueindex > 0 {
                     currentDialogueindex -= 1
-                    
                     showNextDialogue()
                 }
-                
             }
         }
-        
-        
+    }
+
+    func showNextDialogue() {
+        if currentDialogueindex < assets.history.count {
+            guard let historyImage else {return}
+            historyImage.texture = SKTexture(imageNamed: assets.history[currentDialogueindex])
+        } else {
+            // Todas as falas foram exibidas, faça algo aqui, como transição para a próxima cena.
+            if let view = self.view {
+                transactionScene.goToNextLevel(view: view, gameScene: "FirstScene")
+            }
+        }
     }
     
 }
